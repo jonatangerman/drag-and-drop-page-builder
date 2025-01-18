e647e70b31718ebc17b0dedef89b7d7583c32377
Jonathan German <jonatangerman@gmail.com>
2025-01-18 17:41:12 -0400

    Create DraggableBlock component

--- src/App.vue
+++ src/App.vue
@@ -1,3 +1,43 @@
 <template>
-  <div></div>
+  <div id="main-container" class="min-h-screen flex w-full">
+    <div id="blocks-container"
+      class="flex max-w-[350px] w-full shadow-lg gap-5 flex-col p-[20px] pt-[50px]">
+      <h1>Draggable Elements</h1>
+      <div id="blocks" class="w-full h-[100px] flex flex-wrap justify-center items-center gap-5">
+        <DraggableBlock type="text" />
+        <DraggableBlock type="image" />
+      </div>
+    </div>
+    <div id="builder-container" class="flex w-full"></div>
+  </div>
 </template>
+
+<script setup lang="ts">
+  import DraggableBlock from '@/components/DraggableBlock.vue'
+</script>
+
+<style lang="scss" scoped>
+
+  h1 {
+    color: var(--color-black);
+    font-size: 20px;
+    font-weight: bold;
+    width: 100%;
+    padding-bottom: 20px;
+    border-bottom: 2px solid var(--color-accent)
+  }
+
+  #builder-container {
+    background-color: var(--color-white);
+
+    .empty-builder {
+      border-color: var(--color-secondary);
+      color: var(--color-secondary);
+      border-style: dotted;
+    }
+  }
+
+  #blocks-container {
+    background-color: var(--color-secondary);
+  }
+</style>
---
+++ src/components/DraggableBlock.vue
@@ -0,0 +1,44 @@
+<template>
+  <div class="p-2 rounded-[4px] flex-none gap-[10px] cursor-pointer justify-center flex items-center h-[50px] w-[auto] pr-[30px] pl-[30px]" draggable="true">
+    <i :class="icon"></i> {{ text }}
+  </div>
+</template>
+
+<script setup lang="ts">
+import { defineProps, ref } from 'vue';
+
+interface DraggableBlock {
+  type: 'text' | 'image'
+}
+
+const props = defineProps<DraggableBlock>();
+const icon = ref<string>('');
+const text = ref<string>('');
+
+const init = () => {
+  switch (props.type) {
+
+    case 'text':
+      icon.value = 'fa fa-text-width'
+      text.value = 'Text'
+    break;
+
+    case 'image':
+      icon.value = 'fa fa-image'
+      text.value = 'Image'
+    break;
+
+    default:
+      return '';
+  }
+}
+
+  init();
+</script>
+
+<style scoped lang="scss">
+  div {
+    color: var(--color-white);
+    background-color: var(--color-black);
+  }
+</style>
